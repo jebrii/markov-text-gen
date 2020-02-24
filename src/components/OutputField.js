5929a2896ec2401a9d800e06ccb3e86adcee6906
Henry Raschke <hraschke@altair.com>
2020-02-23 23:44:51 -0800

    Added components as anticipated

---
+++ src/components/OutputField.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export default class OutputField extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div style={styles.container}>
+
+      </div>
+    )
+  }
+}
+
+const syles = {
+  container: {
+
+  }
+}
